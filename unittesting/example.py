d547b0fe0f88b750ad5ffb5506beea0017a2be13
alenasf <30708007+alenasf@users.noreply.github.com>
2020-06-28 02:14:23 -0700

    Performing_Unit_Testing

---
+++ unittesting/example.py
@@ -0,0 +1,45 @@
+import unittest
+import requests
+
+
+def power(a,b):
+    return a ** b
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        response = requests.get("http://jsonplaceholder.typicode.com/users", timeout=5)
+        self.response = response
+
+
+    def test_power(self):
+        res = power(2, 4)
+        self.assertEqual(res, 16)
+
+
+    def test_length_of_user_list(self):
+        # response = requests.get("http://jsonplaceholder.typicode.com/users", timeout=5)
+        if self.response.ok:
+            self.assertTrue(len(self.response.json()) == 10)
+        else:
+            self.fail("could not fetch data within %d seconds" % 5)
+
+
+    def test_username(self):
+        # response = requests.get("http://jsonplaceholder.typicode.com/users", timeout=5)
+        if self.response.ok:
+            self.assertTrue(self.response.json()[0]['username'] == 'Bret')
+        else:
+            self.fail("could not fetch data within %d seconds" % 5)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
